ce6984438a4f50a512bc5b6418c0c583afe4964f
qhuydtvt <qhuydtvt@gmail.com>
2016-10-21 12:34:11 +0700

    Removed

--- DemoNetworkingRedo/app/src/main/java/techkids/vn/demonetworkingredo/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package techkids.vn.demonetworkingredo.constants;
-
-/**
- * Created by apple on 10/19/16.
- */
-
-public class Constants {
-    public static final String TUMBLELOG_URL = "https://tumblelog-techkids.herokuapp.com/";
-}
